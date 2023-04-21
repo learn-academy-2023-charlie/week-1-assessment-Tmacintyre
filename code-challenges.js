b06397f7e4ad137c077cfce9d3337a135acddad6
Tmacintyre <Tucker.w.macintyre@gmail.com>
2023-04-21 19:20:35 -0400

    Need to add psuedo and finish concepts

--- code-challenges.js
+++ code-challenges.js
@@ -7,6 +7,12 @@
 // --------------------INSTRUCTOR EXAMPLE: Create a function that takes in two strings and determines which of the two strings has more characters. Use the two sets of test variables provided.
 
 // Pseudo code:
+// Input: Two strings 
+// Output: Shows which of the two strings has more characters than the other
+// Create a function that takes in the two strings
+// Use conditional statements as well as built in method (.length) to determine if string one or string two has more characters
+// Create returns that provide the input with more characters
+// Utilize function call
 
 // Set one:
 const fruit1 = "apple"
@@ -18,9 +24,27 @@ const fruit3 = "cherry"
 const fruit4 = "kiwi"
 // Expected outcome: "cherry"
 
+const moreChars = (string1,string2) => { 
+
+    if(string1.length > string2.length){
+        return string1
+    } else if(string1.length < string2.length) {
+        return string2
+    } else{
+        return "Lengths are equal"
+    }
+}
+
+console.log(moreChars(fruit1, fruit2))
+//output: banana
+console.log(moreChars(fruit3, fruit4))
+//output: cherry
+
+
 // --------------------1) Create a function that takes in a number and determines if the given number is below boiling point, at boiling point, or above boiling point. Boiling point is 212 degrees Fahrenheit. Use the test variables provided below.
 
 // Pseudo code:
+
 
 const temperature1 = 42
 // Expected output: "42 is below boiling point"
@@ -31,6 +55,25 @@ const temperature2 = 350
 const temperature3 = 212
 // Expected output: "212 is at boiling point"
 
+const boilPoint = (theTemp) => {
+
+    if(theTemp > 212){
+        return `${theTemp} is above boiling point`
+    } else if (theTemp < 212){
+        return `${theTemp} is below boiling point`
+    } else{
+        return `${theTemp} is at boiling point`
+    }
+}
+console.log(boilPoint(temperature1))
+// Output: 42 is below boiling point
+console.log(boilPoint(temperature2))
+// Output: 350 is above boiling point
+console.log(boilPoint(temperature3))
+// Output: 212 is at boiling point
+
+
+
 // --------------------2) Create the code that will combine the two arrays and return the length using the test variables provided below.
 
 // Pseudo code:
@@ -39,12 +82,28 @@ const padres1984WorldSeriesRuns = [2, 5, 2, 2, 4]
 const padres1998WorldSeriesRuns = [6, 3, 5, 3]
 // Expected output: 9
 
+let combArray = (padres1984WorldSeriesRuns.concat(padres1998WorldSeriesRuns))
+
+console.log(combArray.length)
+// Output: 9
+
+
 // --------------------3) Create the code that will reverse the letters of a string using the test variable provided below.
 
 // Pseudo code:
 
 const currentCohort = "Charlie 2023"
 // Expected output: "3202 eilrahC"
+
+const revString = (theString) =>{
+
+    return theString.split("").reverse().join("")
+}
+
+console.log(revString(currentCohort))
+// Output: 3202 eilrahC
+
+
 
 // --------------------4) Create the code that will return the last index of the given value from the array using the test variables provided below.
 
@@ -58,6 +117,19 @@ const givenValue1 = 42
 const givenValue2 = 10
 // Expected output: 8
 
+const lastIndexNum = (intValue) =>{
+    
+    return numberOfConnections.lastIndexOf(intValue)
+
+}
+
+console.log(lastIndexNum(givenValue1))
+// Output: 7
+console.log(lastIndexNum(givenValue2))
+// Output: 8
+
+
+
 // --------------------5) Create the code that will sort all the numbers in an array from largest to smallest. Use the test variables provided below.
 
 // Pseudo code:
@@ -67,3 +139,15 @@ const sanDiegoSummerTemperatures = [79, 80, 72, 73, 82, 77, 76]
 
 const sanDiegoWinterTemperatures = [59, 68, 62, 59, 66, 67, 66]
 // Expected output: [68, 67, 66, 66, 62, 59, 59]
+
+const largeToSmall = (arrName) => {
+
+    return arrName.sort().reverse()
+
+}
+
+
+console.log(largeToSmall(sanDiegoSummerTemperatures))
+// Output: [82, 80, 79, 77, 76, 73, 72]
+console.log(largeToSmall(sanDiegoWinterTemperatures))
+// Output: [68, 67, 66, 66, 62, 59, 59]
--- coding-concepts.js
+++ coding-concepts.js
@@ -17,16 +17,16 @@ const colors = ["tangerine", "magenta", "lilac", "daffodil"]
 const cohort = "LEARN 2023"
 // console.log(cohort.length)
 
-// a) Your answer:
-// b) Verify and explain:
+// a) Your answer: 10
+// b) Verify and explain: So the log command is asking for the length of the string "cohort" which contains 9 indexes. The length is equal to the ammount of indexes + 1.
 
 // --------------------2) What will this log?
 
 const greeting = "Hello World!"
 // console.log(greeting[4])
 
-// a) Your answer:
-// b) Verify and explain:
+// a) Your answer: "o"
+// b) Verify and explain: The command is looking to log whatever data is located in the variables index[4]. Since index starts at zero in the fourth index would be the fifth letter in "Hello World!"
 
 // --------------------3) What will this log?
 
@@ -34,8 +34,8 @@ const languages = ["JavaScript", "Ruby", "Python", "C++"]
 const index = 1
 // console.log(languages[index])
 
-// a) Your answer:
-// b) Verify and explain:
+// a) Your answer: "Ruby"
+// b) Verify and explain: Because the variable index is equal to the number 1, the command is equal to console.log(languages[1]). That being said, the command is providing the information located withing the 1st index of the Array "languages".
 
 // --------------------4) What will this log?
 
